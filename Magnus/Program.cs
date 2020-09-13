598d5fe2c91f08df534b61a37ce2061907431027
CYao <cyao2926@gmail.com>
2020-09-13 18:16:22 +1000

    reorganised project and added references into repo

---
+++ Magnus/Program.cs
@@ -0,0 +1,24 @@
+﻿using DatabaseConnection;
+using Include;
+using System;
+using Msg = Include.Json.Message;
+
+namespace Magnus {
+    class Program {
+        static void Main(string[] args) {
+            var mydatabase = new Database();
+            var server = new Server(null, 12345);
+
+            server.OnReceiveListener += (clientId, socketType, dataType, data) => {
+                if (Msg.TryCast(dataType, data, (int)MsgType.Disconnect, out Message _)) {
+
+                }
+            };
+
+            server.Begin();
+
+            //Console.ReadKey();
+            //server.End();
+        }
+    }
+}
---
+++ Magnus/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Magnus")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Magnus")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55c7711c-a159-4600-9ba0-04dffa2de03f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("20.9.13.0810")]
+[assembly: AssemblyFileVersion("20.9.13.0810")]
