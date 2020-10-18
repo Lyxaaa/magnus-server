73648a978ede1b39d5861f143d23fe781d962452
CYao <cyao2926@gmail.com>
2020-10-18 18:16:45 +1000

    Pathfinding and basics for chess board state complete

--- Tangible/Program.cs
+++ Tangible/Program.cs
@@ -1,13 +1,16 @@
 ﻿using Include;
 using Include.Util;
+using Priority_Queue;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
 
 namespace Tangible {
-    enum ChessPiece {
+    enum ChessPiece : byte {
         None = 0,
         BPawn = 1,
         WPawn = 2,
@@ -31,16 +34,79 @@ namespace Tangible {
         public List<(int, int)> Path { get; set; } = new List<(int, int)>();
     }
 
+    class PathNode : FastPriorityQueueNode {
+        public (int, int) Position { get; set; }
+        public (int, int)? BoardPos { get; set; } = null;
+        public ChessPiece Piece { get; set; } = ChessPiece.None;
+        public PathNode Parent { get; set; } = null;
+        public List<PathNode> Adjacent { get; set; } = new List<PathNode>();
+
+        public double GetDistance(PathNode node) {
+            return Math.Sqrt(Math.Pow(node.Position.Item1 - Position.Item1, 2) + Math.Pow(node.Position.Item2 - Position.Item2, 2));
+        }
+
+        public bool IsAdjacent(PathNode node) {
+            return Math.Abs(node.Position.Item1 - Position.Item1) + Math.Abs(node.Position.Item2 - Position.Item2) == 1;
+        }
+
+        public void AddIfAdjacent(PathNode node) {
+            if (IsAdjacent(node)) {
+                Adjacent.Add(node);
+            }
+        }
+    }
+
     class StateDiff {
         public ChessPiece From { get; set; }
         public ChessPiece To { get; set; }
         public int Index { get; set; }
 
-        public bool IsGone { get { return To == ChessPiece.None; } }
-        public bool IsAppear { get { return From == ChessPiece.None; } }
+        // prioritize the piece that is there at the end
+        public ChessPiece Piece { get { return From == ChessPiece.None ? To : From; } }
 
-        public static Move operator +(StateDiff a, StateDiff b) {
-            return new Move() { };
+        // if the from is none, the piece has moved somewhere else
+        public bool IsGone { get { return To == ChessPiece.None; } }
+
+        // if the piece is move, then it spawned out of nowhere
+        public bool IsMove { get { return From == ChessPiece.None; } }
+
+        // if is take, then the piece was replaced by another piece
+        public bool IsTake { get { return To != ChessPiece.None && From != ChessPiece.None; } }
+
+        // 2 valid diffs added make a move, otherwise a null
+        public static Move operator *(StateDiff a, StateDiff b) {
+
+            if (a.Piece != b.Piece) {
+                Log.E($"Move+ : incompatable operands");
+                return null;
+            }
+
+            if (b == a) {
+                Log.E($"Move+ : operands cannot be the same");
+                return null;
+            }
+
+            if (!b.IsMove || !b.IsTake) {
+                Log.E($"Move+ : second operand must be IsMove or IsTake");
+                return null;
+            }
+
+            if (!a.IsGone) {
+                Log.E($"Move+ : first operand must be IsGone");
+                return null;
+            }
+
+            if (!BoardState.TryGetPathPositionFromBoardIndex(a.Index, out var start)) {
+                Log.E($"Move+ : first operand's index invalid");
+                return null;
+            }
+
+            if (!BoardState.TryGetPathPositionFromBoardIndex(b.Index, out var end)) {
+                Log.E($"Move+ : second operand's index invalid");
+                return null;
+            }
+
+            return new Move() { Start = start, End = end, Piece = a.Piece };
         }
     }
 
@@ -54,12 +120,45 @@ namespace Tangible {
         public static readonly int PATHHEIGHT = HEIGHT * 2 + 1;
         public static readonly int PATHSIZE = PATHWIDTH * PATHHEIGHT;
 
+        // coords are in path space
+        public static List<(int, int)> DISCARDZONE = new List<(int, int)>() {
+            (-1, 4),
+            (-1, 5),
+            (-1, 6),
+            (-1, 7),
+            (-1, 8),
+            (-1, 9),
+            (-1, 10),
+            (-1, 11),
+            (-1, 12),
+            (-2, 4),
+            (-2, 5),
+            (-2, 6),
+            (-2, 7),
+            (-2, 8),
+            (-2, 9),
+            (-2, 10),
+            (-2, 11),
+            (-2, 12)
+        };
+
+        // using this as the real discard zone until the robot crashes itself
+        public static List<(int, int)> SAFEDISCARD = new List<(int, int)>() {
+            (-2, 5),
+            (-2, 6),
+            (-2, 7),
+            (-2, 8),
+            (-2, 9),
+            (-2, 10),
+            (-2, 11)
+        };
+
         public static readonly int PRINTPADDING = 1;
 
         ChessPiece[] board = new ChessPiece[SIZE];
-        List<(int, int)> path = new List<(int, int)>();
+        List<PathNode> navMesh = new List<PathNode>();
 
-        Dictionary<ChessPiece, char> symbols = new Dictionary<ChessPiece, char>() {
+        private static readonly Dictionary<ChessPiece, char> symbols = new Dictionary<ChessPiece, char>() {
             { ChessPiece.None    , ' ' },
             { ChessPiece.BPawn   , 'P' },
             { ChessPiece.WPawn   , 'p' },
@@ -169,37 +268,146 @@ namespace Tangible {
             Array.Copy(absoluteStateOfTheBoard, board, SIZE);
         }
 
-        // gets the difference between the 2 boards as a list of moves to be executed
-        public List<Move> Diff(ChessPiece[] absoluteStateOfTheBoard) {
+        public static bool TryGetPathPositionFromBoardIndex((int, int) boardPosition, out (int, int) pathPosition) {
+            int index = boardPosition.Item1 * 2 + 1 + boardPosition.Item2 * PATHWIDTH;
+            return TryGetPathPositionFromBoardIndex(index, out pathPosition);
+        }
+
+        // if out of range, will return false
+        // otherwise, out is the translated position
+        public static bool TryGetPathPositionFromBoardIndex(int index, out (int, int) position) {
+            position = (0, 0);
+            if (index >= 0 && index < SIZE) {
+                position = (index % WIDTH, index / WIDTH);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public (int, int) GetClosestDiscardPoint((int, int) start) {
+            (int, int) closest = (0, 0);
+            double min = float.MaxValue;
+            foreach (var point in SAFEDISCARD) {
+                double hype = Math.Pow(point.Item1 - start.Item1, 2) + Math.Pow(point.Item2 - start.Item2, 2);
+                if (hype < min) {
+                    closest = point;
+                    min = hype;
+                }
+            }
+            return closest;
+        }
+
+        // gets the difference between the newState and current board state as a list of moves to be executed
+        // this doesn't handle every single possible board state a to board state b, but shoudl handle all legal moves
+        // and many illegal ones
+        public List<Move> Diff(ChessPiece[] newState) {
             var moves = new List<Move>();
             var diffs = new List<StateDiff>();
 
             for (int i = 0; i < SIZE; i++) {
-                if (absoluteStateOfTheBoard[i] != board[i]) {
-                    diffs.Add(new StateDiff() { From = board[i], To = absoluteStateOfTheBoard[i], Index = i });
+                if (newState[i] != board[i]) {
+                    diffs.Add(new StateDiff() { From = board[i], To = newState[i], Index = i });
                 }
             }
 
-            List<ChessPiece> dissappearedPieces = new List<ChessPiece>();
-            List<ChessPiece> appearedPieces = new List<ChessPiece>();
+            var gonePieces = diffs.Where(x => x.IsGone).ToList(); // pieces that moved somewhere else, replaced with empty
+            var movePieces = diffs.Where(x => x.IsMove).ToList(); // pieces that moved to a location from somewhere else, empty replaced by a piece
+            var takePieces = diffs.Where(x => x.IsTake).ToList(); // pieces which have displaced other pieces, pieces replaced by another piece
 
-            foreach (var diff in diffs) {
-                if (diff.From == ChessPiece.None) {
-
-                }
-
-                if (diff.From == ChessPiece.None) {
-
+            // remove the pieces that have been taken
+            foreach (var take in takePieces) {
+                if (TryGetPathPositionFromBoardIndex(take.Index, out var position)) {
+                    moves.Add(new Move() { Piece = take.From, Start = position, End = GetClosestDiscardPoint(position) });
+                } else {
+                    Log.E($"Diff: take index{take.Index} is not valid");
                 }
             }
+
+            // move pieces to positions which took pieces
+            foreach (var take in takePieces) {
+                var origin = gonePieces.First(x => x.Piece == take.To);
+                if (origin == null) {
+                    TryGetPathPositionFromBoardIndex(take.Index, out var position);
+                    Log.E($"Diff: could not find match for {take.Piece} moving to {position}");
+                    continue;
+                }
+                gonePieces.Remove(origin);
+                moves.Add(take * origin);
+            }
+
+            //move pieces that just moved
+            foreach (var move in movePieces) {
+                var origin = gonePieces.First(x => x.Piece == move.Piece);
+                if (origin == null) {
+                    TryGetPathPositionFromBoardIndex(move.Index, out var position);
+                    Log.E($"Diff: could not find match for {move.Piece} moving to {position}");
+                    continue;
+                }
+                gonePieces.Remove(origin);
+                moves.Add(move * origin);
+            }
+
+
+            if (gonePieces.Count > 0) Log.E($"Diff: {gonePieces.Count} pieces remain unresolved");
+
+            foreach (var gone in gonePieces) {
+                Log.E($"Diff: {gone.Piece} at {gone.Index}");
+            }
+
+            //path find!
 
             return moves;
         }
 
+        // finds path for move action
+        public Move Resolve(Move move) {
+            FastPriorityQueue<PathNode> queue = new FastPriorityQueue<PathNode>(PATHSIZE + DISCARDZONE.Count);
+            var start = navMesh.First(x => x.BoardPos == move.Start);
+            var end = navMesh.First(x => x.BoardPos == move.End);
+
+            queue.Enqueue(start, (float)start.GetDistance(end));
+
+            while (queue.Count != 0) {
+                var node = queue.Dequeue();
+                foreach (var adj in start.Adjacent) {
+                    adj.Parent = start;
+                    if (adj != end) {
+                        if (adj.Piece == ChessPiece.None)
+                            queue.Enqueue(adj, (float)adj.GetDistance(end));
+                    } else {
+                        break;
+                    }
+                }
+                if (end.Parent != null) break;
+            }
+
+            List<PathNode> nodes = new List<PathNode>();
+            while (end != start) {
+                nodes.Add(end);
+                end = end.Parent;
+                if (end == null) return null;
+            }
+
+            for (int i = nodes.Count - 1; i >= 0; i++) {
+                move.Path.Add(nodes[i].Position);
+            }
+            return move;
+        }
+
         public void PrintState() {
+            PrintState(board);
+        }
+
+        public static void PrintState(ChessPiece[] boardState) {
+            if (boardState.Length != SIZE) {
+                Log.E("PrintState: boardState length does not match");
+                return;
+            }
+
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < SIZE; i++) {
-                sb.Append(symbols[board[i]]);
+                sb.Append(symbols[boardState[i]]);
                 // chuck a new line in there
                 if (i % WIDTH == WIDTH - 1) {
                     sb.Append(Environment.NewLine);
@@ -213,6 +421,7 @@ namespace Tangible {
         }
 
         public bool IsValidPathPos((int, int) position) {
+            if (DISCARDZONE.Contains(position)) return true;
             return position.Item1 >= 0 && position.Item2 >= 0 && position.Item1 < PATHWIDTH && position.Item2 < PATHHEIGHT;
         }
 
@@ -224,6 +433,39 @@ namespace Tangible {
             return str.PadLeft(((PRINTPADDING - str.Length) / 2)
                             + str.Length, pre)
                    .PadRight(PRINTPADDING, post);
+        }
+
+        // just clears all pathfinding flags in the navmesh and updates to latest relevant model
+        public void ResetNavMesh() {
+            foreach (var node in navMesh) {
+                if (node.BoardPos != null) {
+                    node.Piece = GetPiece(((int, int))node.BoardPos);
+                }
+            }
+        }
+
+        // wipes and rebuilds navmesh, this calls ResetNavMesh on its own, no need to call it manually
+        public void RebuildNavMesh() {
+            navMesh = new List<PathNode>();
+            for (int i = 0; i < PATHSIZE; i++) {
+                var node = new PathNode() { Position = (i % PATHWIDTH, i / PATHWIDTH) };
+                if ((i / PATHWIDTH) % 2 == 1 && (i % PATHWIDTH) % 2 == 1) {
+                    node.BoardPos = (i % PATHWIDTH / 2, i / PATHWIDTH / 2);
+                }
+                navMesh.Add(node);
+            }
+
+            foreach (var point in DISCARDZONE) {
+                navMesh.Add(new PathNode() { Position = point });
+            }
+
+            foreach (var point in navMesh) {
+                foreach (var other in navMesh) {
+                    point.AddIfAdjacent(other);
+                }
+            }
+
+            ResetNavMesh();
         }
 
         // checks to see if all points are only 1 apart from eachother
@@ -286,8 +528,6 @@ namespace Tangible {
                     //sb.Append(Pad(i));
                 } else if (path?.Contains(pathPos) ?? false) { // if contains path value
                     char symbol = ' ';
-                    char pre = 'x';
-                    char post = 'y';
 
                     if (path.Contains(pathPos)) {
                         int pos = path.IndexOf(pathPos);
@@ -324,7 +564,7 @@ namespace Tangible {
                         }
                     }
 
-                    sb.Append(Pad(symbol, pre, post));
+                    sb.Append(Pad(symbol));
                 } else { // otherwise it's empty
                     sb.Append(Pad(" "));
                 }
@@ -367,6 +607,10 @@ namespace Tangible {
 
             // on piece move, scan for where pieces are
             socket.OnReceiveListener += Write;
+
+            // scan update boardstate when we get a message
+            Thread write = new Thread(new ThreadStart(DeQueue));
+            write.Start();
 
             // scan update boardstate when we get a message
             Thread read = new Thread(new ThreadStart(Read));
@@ -425,26 +669,36 @@ namespace Tangible {
 
             run = false;
             read.Join();
+            write.Join();
             socket.End();
 
             Log.I("Tangible: Done");
             Console.ReadKey();
         }
 
+       static ConcurrentQueue<byte[]> dataQueue = new ConcurrentQueue<byte[]>();
         static void Write(IPEndPoint endpoint, byte[] data) { // write virtual board state to real board
             Log.D("Got new boardstate from device");
+            dataQueue.Enqueue(data);
+            
             // find difference between two board states
             // remove pieces first
             // while pieces arent where they should be
             // foreach piece
             // if spot is free, move piece to spot
             // wait for input
-            Thread.Sleep(10);
+        }
+
+        static void DeQueue() {
+            while(dataQueue.TryDequeue(out var data)) {
+                ChessPiece[] state = Array.ConvertAll(data, c => (ChessPiece)c);
+                BoardState.PrintState(state);
+            }
         }
 
         static void Read() { // reads real board state and reports to phone
             while (run) {
-                Log.D("Reading new boardstate from board");
+                //Log.D("Reading new boardstate from board");
                 // run opencv
                 // read map
                 // send map
